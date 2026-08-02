7989df73aa2cbb6950939501a8175eb0767ac51f
Ovidiu <eu_oni@yahoo.com>
2026-08-02 20:04:57 +0300

    adding icons on sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,23 @@
-import { NavLink } from "react-router-dom"
-import menu from "../config/menu"
+import { NavLink } from "react-router-dom";
+import menu from "../config/menu";
 
 export default function Sidebar() {
     return (
         <aside>
             <ul>
-                {menu.map((item) => (
-                    <li key={item.id}>
-                        <NavLink to={item.path}>
-                            {item.title}
-                        </NavLink>
-                    </li>
-                ))}
+                {menu.map((item) => {
+                    const Icon = item.icon;
+
+                    return (
+                        <li key={item.id}>
+                            <NavLink to={item.path}>
+                                <Icon />
+                                <span>{item.title}</span>
+                            </NavLink>
+                        </li>
+                    );
+                })}
             </ul>
         </aside>
-    )
+    );
 }
--- src/config/menu.js
+++ src/config/menu.js
@@ -1,43 +1,70 @@
+
+import {
+    LuLayoutDashboard,
+    LuMonitor,
+    LuNetwork,
+    LuWifi,
+    LuBluetooth,
+    LuRadio,
+    LuFileText,
+    LuSettings,
+} from "react-icons/lu";
+
 const menu = [
     {
         id: 1,
         title: "Dashboard",
         path: "/dashboard",
+        icon: LuLayoutDashboard,
     },
     {
         id: 2,
         title: "Devices",
         path: "/devices",
+        icon: LuMonitor,
+
     },
     {
         id: 3,
         title: "Network",
         path: "/network",
+        icon: LuNetwork,
+
     },
     {
         id: 4,
         title: "Wi-Fi",
         path: "/wifi",
+        icon: LuWifi,
+
     },
     {
         id: 5,
         title: "Bluetooth",
         path: "/bluetooth",
+        icon: LuBluetooth,
+
     },
     {
         id: 6,
         title: "SDR",
         path: "/sdr",
+        icon: LuRadio,
+
     },
     {
         id: 7,
         title: "Logs",
         path: "/logs",
+        icon: LuFileText,
+
     },
     {
         id: 8,
         title: "Settings",
         path: "/settings",
+        icon: LuSettings,
+
     },
 ]
 
